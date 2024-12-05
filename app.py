04ab31eb72fd54f1bcfb055cc170110a221b881c
hqbui1301 <hqbui1301@gmail.com>
2024-12-05 20:09:47 +0700

    support edit and save to database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
@@ -14,6 +14,21 @@ def index():
     rows = conn.execute('SELECT * FROM users').fetchall()
     conn.close()
     return render_template('index.html', rows=rows)
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    conn = get_db_connection()
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        conn.execute('UPDATE users SET name = ?, age = ? WHERE id = ?', (name, age, id))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+    else:
+        user = conn.execute('SELECT * FROM users WHERE id = ?', (id,)).fetchone()
+        conn.close()
+        return render_template('edit.html', user=user)
 
 if __name__ == '__main__':
     app.run(debug=True)
